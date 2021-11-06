7d6b31254c97ca8b511d4ce825c88f4b0b56b0b8
邱国辉 <qiuguohui@bytedance.com>
2021-11-06 17:34:06 +0800

    feat: 优化音频播放逻辑

--- pages/piano/index.js
+++ pages/piano/index.js
@@ -4,12 +4,38 @@ Page({
    * 页面的初始数据
    */
   data: {
+    musicNames: [
+      'G3',
+      'Gs3',
+      'A3',
+      'As3',
+      'B3',
+      'C4',
+      'Cs4',
+      'D4',
+      'Ds4',
+      'E4',
+      'F4',
+      'Fs4',
+      'G4',
+      'Gs4',
+      'A4',
+      'As4',
+      'B4',
+      'C5',
+      'Cs5',
+      'D5',
+      'Ds5',
+      'E5',
+      'F5',
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.loadAudio()
   },
 
   /**
@@ -61,25 +87,35 @@ Page({
   },
 
   onButtonTouchstart (ev) {
+    console.log('touchstart');
+    console.log(ev);
     const name = ev.target.dataset.name;
-    var audio = wx.createInnerAudioContext({ useWebAudioImplement: false });
-    audio.src = getUrlByName(name);
+    const audio = createAudioByName(name);
     audio.play();
   },
 
   onButtonTouchend (ev) {
+    console.log('touchend');
     // const name = ev.target.dataset.name;
     // const audio = this.audios[name];
     // audio.stop();
+  },
+
+  loadAudio() {
+    const names = this.data.musicNames;
+    names.forEach(name => {
+      createAudioByName(name);
+    })
   }
 })
+
+function createAudioByName(name) {
+  const audio = wx.createInnerAudioContext({ useWebAudioImplement: true });
+  audio.src = getUrlByName(name);
+  return audio;
+}
 
 function getUrlByName(name) {
   const base = 'https://cdn.jsdelivr.net/gh/warpprism/cdn@latest/autopiano/static/samples/bright_piano/';
   return base + name + '.mp3';
-}
-
-function MusicButton (options) {
-  this.type = this.musicNumber % 1 === 0 ? 'white' : 'black'
-  this.name = options.name;
 }
